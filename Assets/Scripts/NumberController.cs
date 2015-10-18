f25bd5b1b633eaa54c8a2352bd65f5b55673f5f2
Michael Barry <michael.barrys.email@gmail.com>
2015-10-18 16:02:47 +0100

    -implemented random value generation for numbers -implemented left and right sprite generation based on value

---
+++ Assets/Scripts/NumberController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberController : MonoBehaviour
+{
+	
+	//value variables
+	public int value;
+	private int minValue = 1;
+	private int maxValue = 99;
+	
+	//array of sprites for numbers
+	public Sprite[] sprites;
+	
+	//left and right number gameObjects
+	public GameObject leftNum;
+	public GameObject rightNum;
+	
+	//left and right number spriteRenderers and sprites for those renderers
+	private SpriteRenderer leftRenderer;
+	private SpriteRenderer rightRenderer;
+	private Sprite leftSprite;
+	private Sprite rightSprite;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		//set the value randomly based on the minimum and maximum possible values
+		value = Random.Range(minValue, maxValue);
+		
+		//set the sprites for the left and right numbers based on the value
+		setNumberSprites();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	
+	
+	//method to set the left and right number sprites based on the value
+	private void setNumberSprites()
+	{
+		//get the left and right spriteRenderers
+		leftRenderer = leftNum.GetComponent<SpriteRenderer>() as SpriteRenderer;
+		rightRenderer = rightNum.GetComponent<SpriteRenderer>() as SpriteRenderer;
+		
+		//figure out how many 10s and 1s are in the value
+		int leftSpriteIndex = value / 10;
+		int rightSpriteIndex = value % 10;
+		
+		//get the correct left sprite and set the spriteRenderer sprite to it
+		leftSprite = sprites[leftSpriteIndex];
+		leftRenderer.sprite = leftSprite;
+		
+		//get the correct right sprite and set the spriteRenderer sprite to it
+		rightSprite = sprites[rightSpriteIndex];
+		rightRenderer.sprite = rightSprite;
+	}
+	
+}
