742b045b707b7065241f6070c78b5f9fd29854bc
Michal Gnacek <m.gnacek@kainos.com>
2015-10-24 00:07:21 +0100

    Add moving number for testing, alternative buttons for rotation (a/d), collision detection along with PlayerValue being calculated on collisions

--- Assets/Scripts/NumberController.cs
+++ Assets/Scripts/NumberController.cs
@@ -21,6 +21,9 @@ public class NumberController : MonoBehaviour
 	private SpriteRenderer rightRenderer;
 	private Sprite leftSprite;
 	private Sprite rightSprite;
+
+	//player GameObject
+	private GameObject player;
 	
 	// Use this for initialization
 	void Start () 
@@ -30,12 +33,32 @@ public class NumberController : MonoBehaviour
 		
 		//set the sprites for the left and right numbers based on the value
 		setNumberSprites();
+
+		//Find the player GameObject
+		player = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+
+		//If number collides with player, perform correct action based on the side collided with
+		if(coll.gameObject.tag == "Add"){
+			player.GetComponent<PlayerController>().addPlayerValue(value);
+		}
+		else if(coll.gameObject.tag == "Subtract"){
+			player.GetComponent<PlayerController>().subtractPlayerValue(value);
+		}
+		else if(coll.gameObject.tag == "Multiply"){
+			player.GetComponent<PlayerController>().multiplyPlayerValue(value);
+		}
+		else if(coll.gameObject.tag == "Divide"){
+			player.GetComponent<PlayerController>().dividePlayerValue(value);
+		}
 	}
 	
 	
