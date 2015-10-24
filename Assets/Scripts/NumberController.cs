1cb1a89592fb74f9a1b662be96f4c3adfd71047f
Michael Barry <michael.barrys.email@gmail.com>
2015-10-24 01:38:10 +0100

    Checked merge

--- Assets/Scripts/NumberController.cs
+++ Assets/Scripts/NumberController.cs
@@ -4,39 +4,30 @@ using System.Collections;
 public class NumberController : MonoBehaviour
 {
 	
-	//value variables
+	// value variables
 	public int value;
-	private int minValue = 1;
-	private int maxValue = 99;
-	
-	//array of sprites for numbers
-	public Sprite[] sprites;
-	
-	//left and right number gameObjects
-	public GameObject leftNum;
-	public GameObject rightNum;
-	
-	//left and right number spriteRenderers and sprites for those renderers
-	private SpriteRenderer leftRenderer;
-	private SpriteRenderer rightRenderer;
-	private Sprite leftSprite;
-	private Sprite rightSprite;
+	private int minValue = 1000;
+	private int maxValue = 9999;
+
+	// getter for value
+	public int getValue()
+	{
+		return value;
+	}
 
 	//player GameObject
 	private GameObject player;
-	
+
 	// Use this for initialization
 	void Start () 
 	{
-		//set the value randomly based on the minimum and maximum possible values
+		// set the value randomly based on the minimum and maximum possible values
 		value = Random.Range(minValue, maxValue);
-		
-		//set the sprites for the left and right numbers based on the value
-		setNumberSprites();
 
 		//Find the player GameObject
 		player = GameObject.Find("Player");
 	}
+	
 	
 	// Update is called once per frame
 	void Update () 
@@ -59,27 +50,6 @@ public class NumberController : MonoBehaviour
 		else if(coll.gameObject.tag == "Divide"){
 			player.GetComponent<PlayerController>().dividePlayerValue(value);
 		}
-	}
-	
-	
-	//method to set the left and right number sprites based on the value
-	private void setNumberSprites()
-	{
-		//get the left and right spriteRenderers
-		leftRenderer = leftNum.GetComponent<SpriteRenderer>() as SpriteRenderer;
-		rightRenderer = rightNum.GetComponent<SpriteRenderer>() as SpriteRenderer;
-		
-		//figure out how many 10s and 1s are in the value
-		int leftSpriteIndex = value / 10;
-		int rightSpriteIndex = value % 10;
-		
-		//get the correct left sprite and set the spriteRenderer sprite to it
-		leftSprite = sprites[leftSpriteIndex];
-		leftRenderer.sprite = leftSprite;
-		
-		//get the correct right sprite and set the spriteRenderer sprite to it
-		rightSprite = sprites[rightSpriteIndex];
-		rightRenderer.sprite = rightSprite;
 	}
 	
 }
---
+++ Assets/Scripts/NumberSprites/NumberSpriteGeneratorController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberSpriteGeneratorController : MonoBehaviour
+{
+
+	// the number gameobject
+	private GameObject parentNumberObj;
+	
+	//the value of the number gameobject
+	public int value;
+	
+	public Sprite[] numberSpriteArray;
+	
+	public float offset;
+	
+	public bool created = false;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		// set the parentNumberObj
+		parentNumberObj = transform.parent.gameObject;
+		
+		// get value from the parentNumberObj
+		value = parentNumberObj.GetComponent<NumberController>().getValue();
+		
+		
+		CreateNumberSprites();
+	}
+	
+	
+	// method to create the required numberSprites
+	void CreateNumberSprites()
+	{
+	    // put the value into a character array so the individual characters can be accessed
+		char[] numbers = value.ToString().ToCharArray();
+		
+		// store the current position
+		Vector3 newPos = transform.position;
+		
+		// if there are even amount of numbers
+		if((numbers.Length % 2) == 0)
+		{
+			for(int i = (numbers.Length / 2) - 1; i >= 0; i--)
+			{
+				//decrease the newpos x position by the offset
+				newPos.x -= offset;
+				
+				//instantiate a new numberSprite gameObject
+				GameObject numberSprite = (GameObject)Instantiate(Resources.Load("Prefabs/NumberSprites/NumberSprite"), newPos, transform.rotation);
+				
+				numberSprite.transform.parent = gameObject.transform;
+				
+				numberSprite.GetComponent<NumberSpriteController>().setSprite(numberSpriteArray[(int)char.GetNumericValue(numbers[i])]);
+				
+				newPos.x -= offset;
+			}
+			
+			// reset the newPos
+			newPos = transform.position;
+			
+			for(int i = (numbers.Length / 2); i < numbers.Length; i++)
+			{
+				//decrease the newpos x position by the offset
+				newPos.x += offset;
+				
+				//instantiate a new numberSprite gameObject
+				GameObject numberSprite = (GameObject)Instantiate(Resources.Load("Prefabs/NumberSprites/NumberSprite"), newPos, transform.rotation);
+				
+				numberSprite.transform.parent = gameObject.transform;
+				
+				numberSprite.GetComponent<NumberSpriteController>().setSprite(numberSpriteArray[(int)char.GetNumericValue(numbers[i])]);
+				
+				newPos.x += offset;
+			}
+		}
+		else
+		{
+			// odd count of numbers here!!
+		}
+
+	}
+
+
+
+
+
+
+
+}
